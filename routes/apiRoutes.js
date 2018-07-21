f90a2ed64dec00813b199f34cfa9857658cfac9a
Amanda Kay <amandakaywiggins@gmail.com>
2018-07-21 13:22:21 -0500

    made new functions in apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -23,15 +23,37 @@ module.exports = function(app) {
   });
 
   // Get all expenses for a tab
+  app.get("api/expenses/:tabid", function(req,res) {
+    db.Expense.findAll({
+      where: { tabid: req.params.tabid} }).then(function(dbExpense) {
+        res.json(dbExpense);
+      });
+    });
 
   // Add a new expense to a tab
+  app.post("/api/expenses", function(req, res) {
+    db.Expense.create(req.body).then(function(dbExpense) {
+      res.json(dbExpense);
+    });
+  });
 
   // Modify an expense by id
 
   // Delete an expense by id
+  app.delete("/api/expenses/:id", function(req, res) {
+    db.Expense.destroy({ where: { id: req.params.id } }).then(function(dbExpense) {
+      res.json(dbExpense);
+    });
+  });
 
   // Get all expenses added by username
-  
+  app.get("/api/expenses/:username", function(req, res) {
+    db.Expense.findAll({
+      where: { username: req.params.username} }).then(function(dbExpense) {
+        res.json(dbExpense);
+      });
+    });
+
   //Get all expenses NOT added by username
 
   //Total of expenses added by username
